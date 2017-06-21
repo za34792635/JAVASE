8fb3b9594c5074f02b9aba457361b4854d2bd2f7
za34792635 <380366595@qq.com>
2017-06-21 16:53:38 +0800

    Hibernate配置文件

--- src/com/qyspeed/main/Main.java
+++ src/com/qyspeed/main/Main.java
@@ -1,14 +1,56 @@
 package com.qyspeed.main;
 import com.qyspeed.test.TestRunnian;
+
+import java.sql.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.qyspeed.domain.Admin;
 import com.qyspeed.test.TestGrade;
 public class Main {
 
 	public static void main(String[] args)
-	{
+	{/*
 		TestRunnian r=new TestRunnian();
 		r.runnian();
 		
 		TestGrade point=new TestGrade();
 		point.Grade(90.6);
+		*/
+		  //读取配置文件  
+        Configuration cfg = new Configuration().configure();  
+          
+        SessionFactory factory = cfg.buildSessionFactory();  
+          
+        Session session = null;  
+        try{  
+            session = factory.openSession();  
+            //开启事务  
+            session.beginTransaction();  
+              
+            Admin user = new Admin();  
+            user.setUsername("dengjiajun");  
+            user.setPassword("123456");  
+             
+              
+            session.save(user);  
+            //提交事务  
+            session.getTransaction().commit();  
+              
+        }catch(Exception e){  
+            e.printStackTrace();  
+            //回滚事务  
+            session.getTransaction().rollback();  
+        }finally{  
+            if(session != null){  
+                if(session.isOpen()){  
+                    //关闭session  
+                    session.close();  
+                }  
+            }  
+        }  
+    }  
 	}
-}
+
